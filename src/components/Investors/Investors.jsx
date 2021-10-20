b5c4c3cdd3bed80c2e36f32ee74b80d3b61b7821
Salik <salik.ansari6@gmail.com>
2021-10-20 17:23:18 +0530

    added investors and compressed images

--- src/components/Investors/Investors.jsx
+++ src/components/Investors/Investors.jsx
@@ -6,18 +6,24 @@ const Investors = () => {
       <p className=" text-green-700 font-bold text-xl text-center lg:text-4xl">
         Investors and Partners
       </p>
-      <div className="grid grid-cols-3 lg:grid-cols-4 lg:mt-16 place-items-center">
+      <div className="grid grid-cols-2 md:grid-cols-3 lg:mt-16 place-items-center">
         <div className="p-5">
-          <img src="/images/erc20.png" width="100" alt="" />
+          <img src="/images/alphabetty_logo.png" width="150" alt="" />
         </div>
         <div className="p-5">
-          <img src="/images/erc20.png" width="100" alt="" />
+          <img src="/images/coolvox_logo.png" width="150" alt="" />
         </div>
         <div className="p-5">
-          <img src="/images/erc20.png" width="100" alt="" />
+          <img src="/images/eth-men_logo.png" width="150" alt="" />
         </div>
         <div className="p-5">
-          <img src="/images/erc20.png" width="100" alt="" />
+          <img src="/images/nftcomics_logo.png" width="150" alt="" />
+        </div>
+        <div className="p-5">
+          <img src="/images/polygon_logo.png" width="200" alt="" />
+        </div>
+        <div className="p-5">
+          <img src="/images/weaponized_countries_logo.png" width="150" alt="" />
         </div>
       </div>
     </div>
